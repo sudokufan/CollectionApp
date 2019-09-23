772f3d8a2f5e6b01fcc51f3bec5b69507659005f
sudokufan <alexrichardgeorge@protonmail.com>
2019-09-23 16:14:00 +0100

    added styles/normalize files

--- index.php
+++ index.php
@@ -10,10 +10,14 @@ $releaseDate = pullSetRelease($db);
 
 ?>
 
-<html>
-<head>
-    <body>
+<html lang="en-GB">
+    <head>
+        <link rel="stylesheet" type="text/css" href="normalize.css" />
+        <link rel="stylesheet" type="text/css" href="styles.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title></title>
+    </head>
+        <body>
 
-</body>
-</head>
+        </body>
 </html>
