f8acdafca966529799b6968aec5abda0cea91c11
sudokufan <alexrichardgeorge@protonmail.com>
2019-09-23 16:22:31 +0100

    added nav to index

--- index.php
+++ index.php
@@ -15,9 +15,11 @@ $releaseDate = pullSetRelease($db);
         <link rel="stylesheet" type="text/css" href="normalize.css" />
         <link rel="stylesheet" type="text/css" href="styles.css" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title></title>
+        <title>SetCollector</title>
     </head>
         <body>
+
+        <p class="container">SetCollector</p>
 
         </body>
 </html>
