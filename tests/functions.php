1bd8d60e0d910fa601a4c767b4e5c3dd60e46295
sudokufan <alexrichardgeorge@protonmail.com>
2019-09-24 13:35:03 +0100

    copied in placeholder test functions, filled in SuccessTest

--- tests/functions.php
+++ tests/functions.php
@@ -5,9 +5,44 @@ use PHPUnit\Framework\TestCase;
 
 class StackTest extends TextCase
 {
-    public function testSuccessFunction() {
+    public function testSuccessDisplaySetData() {
+        $expected = '<div class="set"> <h1>' . $set['name'] . '</h1>
+                     <h2> Release Date: ' . $set['released'] . '</h2>
+                     <h2>' . $set['cards'] . ' cards</h2>
+                     </div>';
+        $input = [['field'=>'value',]];
+        $case = displaySetCollection($input);
+        $this->assertEquals($case, $expected);
+    }
+
+    public function testFailureDisplaySetData() {
         $expected = ;
         $input = ;
-        $case =
+        $case = displaySetCollection($input);
+        $this->assertEquals($case, $expected);
+    }
+
+    public function testMalformDisplaySetData() {
+        $expected = ;
+        $input = ;
+        $case = displaySetCollection($input);
+        $this->assertEquals($case, $expected);
     }
 }
+
+
+    }
+
+    public function testFailureDisplayCollection() {
+        $inputTestCollection = ['Yo Waddup'];
+        $expectedOutput = 'Error generating HTML from collection';
+
+        $case = displayCollection($inputTestCollection);
+        $this->assertEquals($case, $expectedOutput);
+    }
+
+    public function testMalformedDisplayCollection() {
+        $input = "hello";
+        $this->expectException(TypeError::class);
+        $case = displayCollection($input);
+
