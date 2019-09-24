925c1b88f33bec9108c2a2f6878250937b3f4cef
sudokufan <alexrichardgeorge@protonmail.com>
2019-09-24 13:20:47 +0100

    prepared successtest for functions doc

--- tests/functions.php
+++ tests/functions.php
@@ -1,3 +1,13 @@
 <?php
 
 require_once ('../functions.php');
+use PHPUnit\Framework\TestCase;
+
+class StackTest extends TextCase
+{
+    public function testSuccessFunction() {
+        $expected = ;
+        $input = ;
+        $case =
+    }
+}
