7433739ce0c066ff70da92e0f23bd067ad0a1f01
sudokufan <alexrichardgeorge@protonmail.com>
2019-09-24 13:10:12 +0100

    added testing folder

---
+++ tests/index.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once('functions.php');
+
+$db = connectDB();
+
+$sets = retrieveCardSets($db);
+
+?>
+
+<html lang="en-GB">
+    <head>
+        <link rel="stylesheet" type="text/css" href="normalize.css" />
+        <link rel="stylesheet" type="text/css" href="styles.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>SetCollector</title>
+    </head>
+    <body>
+
+        <nav class="container">
+                <h1>SetCollector</h1>
+        </nav>
+
+
+        <div class="setsBody" class="container">
+
+            <?php echo displaySetData($sets); ?>
+
+    </body>
+</html>
