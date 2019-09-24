ba8ad563a7eee29f91723ee33aa1eedab1ac01a3
sudokufan <alexrichardgeorge@protonmail.com>
2019-09-24 11:44:18 +0100

    added return typehinting to PDO function

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 /**
  * connects SQL database to PHP
  */
-function connectDB()
+function connectDB() :PDO
 {
     $db = new PDO ('mysql:host=db; dbname=AlexCollection', 'root', 'password');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
