5d91149642301d3e7a72588a036ec1765cccd6bc
sudokufan <alexrichardgeorge@protonmail.com>
2019-09-24 13:48:39 +0100

    written three test functions

--- tests/functions.php
+++ tests/functions.php
@@ -5,7 +5,7 @@ use PHPUnit\Framework\TestCase;
 
 class StackTest extends TextCase
 {
-    public function testSuccessDisplaySetData() {
+    public function testSuccessDisplaySetCollection() {
         $expected = '<div class="set"> <h1>' . $set['name'] . '</h1>
                      <h2> Release Date: ' . $set['released'] . '</h2>
                      <h2>' . $set['cards'] . ' cards</h2>
@@ -15,34 +15,17 @@ class StackTest extends TextCase
         $this->assertEquals($case, $expected);
     }
 
-    public function testFailureDisplaySetData() {
-        $expected = ;
-        $input = ;
+    public function testFailureDisplaySetCollection() {
+        $expected = 'Error generating HTML';
+        $input = ['Yu-Gi-Oh set'];
         $case = displaySetCollection($input);
         $this->assertEquals($case, $expected);
     }
 
-    public function testMalformDisplaySetData() {
-        $expected = ;
-        $input = ;
-        $case = displaySetCollection($input);
-        $this->assertEquals($case, $expected);
-    }
-}
-
-
-    }
-
-    public function testFailureDisplayCollection() {
-        $inputTestCollection = ['Yo Waddup'];
-        $expectedOutput = 'Error generating HTML from collection';
-
-        $case = displayCollection($inputTestCollection);
-        $this->assertEquals($case, $expectedOutput);
-    }
-
-    public function testMalformedDisplayCollection() {
-        $input = "hello";
+    public function testMalformedDisplaySetCollection() {
+        $input = 'Ixalan, 289, 2018-04-27';
         $this->expectException(TypeError::class);
-        $case = displayCollection($input);
+        displaySetCollection($input);
+    }
 
+}
