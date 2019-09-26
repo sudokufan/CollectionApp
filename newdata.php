fc9cac82e7e3c77bdd4052a687bcf521ae1f2e11
sudokufan <alexrichardgeorge@protonmail.com>
2019-09-26 11:54:15 +0100

    wrapped everything on newdata.php in if statement to check user input

--- newdata.php
+++ newdata.php
@@ -6,12 +6,19 @@ $db = connectDB();
 
 $newSet = $_POST;
 
-$thing = checkUserInput($newSet);
+$userChecked = checkUserInput($newSet);
 
-$errorCheck = addNewSet($newSet, $db);
+if ($userChecked = true) {
 
-if ( $errorCheck === false) {
-    header('Location: index.php?error=wrongUser');
-} else {
-    header('Location: index.php');
+    $errorCheck = addNewSet($newSet, $db);
+
+    if ($errorCheck === false) {
+        header('Location: index.php?error=wrongUser');
+    } else {
+        header('Location: index.php');
+    }
+
+    else {
+        header('Location: index.php?error=wrongUser');
+    }
 }
